19f7d40b0e26460bc482f0a5ff413b68729d0262
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2021-04-15 08:32:13 +0300

    [lesson1] feat: created custome button: 'toggleButton'

--- src/js/components/main/main.js
+++ src/js/components/main/main.js
@@ -1,46 +1,108 @@
 import { DATA_USERS, COUNTRIES } from "../../data/data";
 
-console.log(DATA_USERS)
-
 const webix = require("webix/webix.js");
+
+const customButton = {
+  view:"toggleButton", 
+  width:100,
+  state:0,
+  states: {0: "Off", 1: "Asc", 2: "Desc"},
+  on:{
+    onStateChange:function(state){
+      switch(state){
+        case 0:
+          $$("namesList").sort("#id#", "asc", "int");
+          break;
+        case 1:
+          $$("namesList").sort("#name#", "asc",);
+          break;
+        case 2:
+          $$("namesList").sort("#name#", "desc");
+          break;
+      }
+    }
+  }
+}
+
+webix.protoUI({
+  name:"toggleButton",
+  $init: function(config){
+    config.value = config.states[0],
+    this.$view.className = "off",
+
+    this.attachEvent("onItemClick", function(){
+      const lastState = 3;
+      const startState = 0;
+    
+      config.state += 1;
+
+      if(config.state === lastState){
+        config.state = startState
+      }
+  
+      this.config.state = config.state;
+      this.config.value = this.config.states[config.state];
+      this.refresh();
+
+      setBtnColor(this);
+      this.callEvent("onStateChange", [this.config.state]);
+    })
+  }
+}, webix.ui.button);
+
+
+const toolBar = {
+  view:"toolbar", 
+  cols:[
+    { view:"label", label:"Sort list:", width:100, align:"center"},
+    customButton
+  ],
+}
+
+const listWithNames = {
+  view:"list",
+  id:"namesList",
+  data:DATA_USERS,
+  template:"#id#. #name#"
+}
+
+const form = {
+  view:"list", data:COUNTRIES, template:"#value#"
+}
 
 const main = {
   rows:[
-    { view:"list", data:DATA_USERS, menu:[ "Add", "Update", "Delete" ],  template:"#name#",},
+    toolBar,
+    listWithNames,
     {view:"resizer"},
-    { view:"list", data:DATA_USERS, menu:"Delete", template:"#country#" }
+    form
   ]
-};
+}
 
-webix.protoUI({
-	name:"menuList",
-    on_context:true,
-  	defaults:{
-        select:true
-    },
-    $init:function(config){
-      	this.$view.className += " menu_list";
-      	this.$ready.push(this.initMenu);
-        this.attachEvent("onAfterContextMenu", function(id){
-        	this.select(id);
-        });
-    },
-    initMenu:function(){
-    	var menu = webix.ui({
-        	view:"contextmenu",
-            data:this.config.menu
-        });
-      	menu.attachTo(this);
-        this.menu = menu.config.id;
-    },
-    getMenu:function(){
-      return this.menu;
-    },
-    menu_setter:function(value){
-  		if(!webix.isArray(value))
-          value = [value];
-        return value;
-    }
-}, webix.ui.list);
+function setBtnColor(btn){
+  const state = btn.config.state;
+
+  if(state === 0){
+    btn.$view.className = "off"
+  }else if(state === 1){
+    btn.$view.className = "asc"
+  }else if(state === 2){
+    btn.$view.className = "desc"
+  }
+}
+
+
 
 export { main }
+
+// switch(state) {
+//   case 0:
+//     btn.css = "off";
+//     break;
+//   case 1:
+//     btn.css = "asc";
+//     break;
+//   case 2:
+//     btn.css = "desc";
+//     break;
+// }
