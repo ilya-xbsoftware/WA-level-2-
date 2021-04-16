50288d3d6947633f822a19b00c287d755f718501
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2021-04-16 17:08:05 +0300

    fix: fixed custome button

--- src/js/components/uiCustome/toggleButton.js
+++ src/js/components/uiCustome/toggleButton.js
@@ -5,18 +5,18 @@ export default webix.protoUI({
   $init: function(config){
     config.value = "Off";
     
-    this._states = config.states;
     this._startState = config.state;
-    this._lastState =  Object.keys(this._states).length;
+    this._lastState =  Object.keys(config.states).length;
     this.$view.classList.add("off");
 
     this.attachEvent("onItemClick", function(){
       this.config.state += 1;
+      
       if(this.config.state === this._lastState){
         this.config.state = this._startState
       }
 
-      this.config.value = this._states[this.config.state];
+      this.config.value = config.states[this.config.state];
       this.refresh();
 
       this._setBtnColor();
@@ -25,12 +25,12 @@ export default webix.protoUI({
   },
   _setBtnColor(){
     const state = this.config.state;
-    const _states = this._states;
+    const states = this.config.states;
     const className = this.$view.classList;
 
-    Object.values(_states).map((singleState)=>{
+    Object.values(states).map((singleState)=>{
       className.remove(singleState.toLowerCase());
     });
-    className.add(_states[state].toLowerCase());
+    className.add(states[state].toLowerCase());
   }
 }, webix.ui.button);
