3f5d24afe589192b1d59935dd30cca962ab132ca
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2021-04-16 16:47:16 +0300

    fix: fixed custome button

--- src/js/components/uiCustome/toggleButton.js
+++ src/js/components/uiCustome/toggleButton.js
@@ -4,34 +4,33 @@ export default webix.protoUI({
   name:"toggleButton",
   $init: function(config){
     config.value = "Off";
-  
-    this.states = config.states;
-    this.state = config.state;
-    this.startState = config.state;
-    this.lastState =  Object.keys(this.states).length;
+    
+    this._states = config.states;
+    this._startState = config.state;
+    this._lastState =  Object.keys(this._states).length;
     this.$view.classList.add("off");
 
     this.attachEvent("onItemClick", function(){
-      this.state += 1;
-      if(this.state === this.lastState){
-        this.state = this.startState
+      this.config.state += 1;
+      if(this.config.state === this._lastState){
+        this.config.state = this._startState
       }
 
-      this.config.value = this.states[this.state];
+      this.config.value = this._states[this.config.state];
       this.refresh();
 
       this._setBtnColor();
-      this.callEvent("onStateChange", [this.state]);
+      this.callEvent("onStateChange", [this.config.state]);
     })
   },
   _setBtnColor(){
-    const state = this.state;
-    const states = this.states;
+    const state = this.config.state;
+    const _states = this._states;
     const className = this.$view.classList;
 
-    Object.values(states).map((singleState)=>{
+    Object.values(_states).map((singleState)=>{
       className.remove(singleState.toLowerCase());
     });
-    className.add(states[state].toLowerCase());
+    className.add(_states[state].toLowerCase());
   }
 }, webix.ui.button);
